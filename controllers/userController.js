0691251bdf14a81d0a64bf166131b1a14f477d83
KumarAyush6265 <139481272+KumarAyush6265@users.noreply.github.com>
2023-07-26 03:16:01 +0530

    Add files via upload

---
+++ controllers/userController.js
@@ -0,0 +1,78 @@
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken")
+//@desc Register a user
+//@route POST /api/users/register
+//@access public
+
+const registerUser = asyncHandler(async  (req,res) => {
+    const {username, email , password } =req.body;
+    if(!username || !email || !password) {
+       res.status(400);
+       throw new Error("All fields are mandatory");
+    }
+    const userAvaliable = await User.findOne(email);
+    if(userAvaliablea) {
+        res.status(400);
+       throw new Error("User already registered");
+    }
+    //Hash Password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("Hashed Password: ", hashedPassword);
+    const user = await User.create({
+        username,
+        email, 
+        password: hashedPassword,
+    });
+
+    console.log(`User created ${user}`);
+    if(user) {
+        res.status(201).json({_id: user.id, emai: user.email}); 
+    }else{
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
+    res.json({message: "Register the User"});
+});
+
+//@desc Login a user
+//@route POST /api/users/login
+//@access public
+
+const loginUser = asyncHandler(async  (req,res) => {
+    const  {email ,password} = req.body;
+    if(!email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    const user = await User.findOne({email });
+    //compare password with hashedpassword
+    if(user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id,
+            },
+        }, process.env.ACCESS_TOKEN_SECRET,
+           { expiresIn: "15m" }
+        );
+        res.status(200).json({ accessToken });
+    }else{
+        res.status(401)
+        throw new Error("email or password is not valid");
+    }
+    res.json({message: "Login the User"});
+});
+
+//@desc Current user info
+//@route POST /api/users/currentuser
+//@access private
+
+const currentUser = asyncHandler(async  (req,res) => {
+    res.json(req.user);
+});
+
+
+module.exports ={registerUser, loginUser, currentUser};
